8536bf4b9b107a45c0916c9a81be8f5ef0a9e2f3
Landford Gabriel Murillo <lgabriel@b7-21.local>
2015-08-14 11:31:02 -0600

    despliega precio de entradas compradas

--- thanks.php
+++ thanks.php
@@ -7,21 +7,33 @@
 	require_once( "classes/concertClass.php" );
 	include( "views/header.php" );
 
+	$id = $_GET['id'];
 	$name = $_POST['name'];
 	$userId = $_POST['userId'];
 	$email = $_POST['email'];
 	$ticket = $_POST['entradas'];
+	
 
 	$concert = new drawConcertTable();
 
+	$concertAll = $concert->eventInfo($id);
+	$print = ($concertAll[0]['precioConcierto']);
+
 	//Get the quantity of tickets in the table ENTRADA
-	$itemsQuant = $concert->countTicketsItems( $_GET['id'] )->count;
+	$itemsQuant = $concert->countTicketsItems( $id )->count;
 	
 	for ( $i=0; $i < $ticket; $i++ ) { 
 		$insertTicket = $concert->insertEntrada( $itemsQuant + $i, $_GET['id'] );
 	}
 
 	$expectator = $concert->insertEspectador( $userId, $name, $email, $ticket, $_GET['id'] );
+
+	echo'
+		<div class="col-md-6">
+		<p>El total a pagar por las '.$ticket.' entradas es de: &#8353 '.( $print * $ticket ).'.</p> 
+			<p>Gracias por su compra</p>
+		</div>
+	';
 	
 	include( "views/footer.php" );
 ?>
